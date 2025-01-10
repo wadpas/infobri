1b282059e5e45975cebcb82d66afa0acc986134f
wadpas <wadpas@gmail.com>
2025-01-10 15:20:11 +0200

    Sidebar comp. added

---
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { LockIcon } from "lucide-react";
+import Image from "next/image";
+import React, { useState } from "react";
+
+const Sidebar = () => {
+  const [showProjects, setShowProjects] = useState(true);
+  const [showPriority, setShowPriority] = useState(true);
+
+  return (
+    <div
+      className={`fixed z-40 flex h-full flex-col justify-between overflow-y-auto bg-white shadow-xl transition-all duration-300 dark:bg-black ${false ? "hidden w-0" : "w-64"}`}
+    >
+      <div className="flex h-[100%] w-full flex-col justify-start">
+        {/* TOP LOGO */}
+        <div className="z-50 flex min-h-[56px] w-64 items-center justify-between bg-white px-8 py-5 dark:bg-black">
+          <Image src="/infobri.svg" alt="Logo" width={200} height={0} />
+        </div>
+        {/* TEAM */}
+        <div className="flex items-center gap-5 border-y-[1.5px] border-gray-200 px-8 py-4 dark:border-gray-700">
+          <Image src="/logo.png" alt="Logo" width={40} height={40} />
+          <div>
+            <h3 className="text-md font-bold tracking-wide dark:text-gray-200">
+              EARTH TEAM
+            </h3>
+            <div className="mt-1 flex items-start gap-2">
+              <LockIcon className="mt-[0.1rem] h-3 w-3 text-gray-500 dark:text-gray-400" />
+              <p className="text-xs text-gray-500">Private</p>
+            </div>
+          </div>
+        </div>
+        {/* NAVBAR LINKS */}
+        <nav className="z-10 w-full"></nav>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
