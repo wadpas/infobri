6bb92f4530672070d40f89b9359eede7031d7b49
wadpas <wadpas@gmail.com>
2025-01-11 19:02:18 +0200

    installed shadcn

---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt', '@nuxtjs/color-mode'],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui',
+  },
+});
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>Hello</div>
+  <Button>Click</Button>
+</template>
+
+<script setup lang="ts"></script>
